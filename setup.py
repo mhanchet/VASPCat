329aad0e7688a325cfbdf1c5f72250a19509ab4c
mcarl15 <mcarl15@ksu.edu>
2014-06-23 15:23:00 -0500

    Update setup.py
    Add new version number '0.1a2' to indicate start of work on Bug #1.
    In addition, add lines with project website and license information.

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'vaspcat',
-    version = '0.1a1',
+    name = 'VaspCat',
+    version = '0.1a2',
 	author = 'Michael Carlson',
-    
+    url = 'https://github.com/mcarl15/VaspCat',
+    license = 'GNU v3',
+
     #Default root is same directory as setup.py
     packages = find_packages(exclude=['*.test']),
     include_package_data = True,
